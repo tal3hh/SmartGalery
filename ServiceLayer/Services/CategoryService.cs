6f3bf6488ca5d26768e2b0e62ad391a126a70941
tal3hh <suleymanovtaleh887@gmail.com>
2024-01-05 20:42:05 +0400

    category service add

---
+++ ServiceLayer/Services/CategoryService.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using DomainLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Contexts;
+using ServiceLayer.Dtos.Category;
+using ServiceLayer.Services.Interfaces;
+
+namespace ServiceLayer.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CategoryService(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        public async Task<List<CategoryDto>> GetAllAsync()
+        {
+            List<Category> list = await _context.Categories.ToListAsync();
+
+            return _mapper.Map<List<CategoryDto>>(list);
+        }
+
+
+        public async Task<CategoryDto> GetByIdAsync(int id)
+        {
+            Category entity = await _context.Categories.FindAsync(id);
+
+            return _mapper.Map<CategoryDto>(entity);
+        }
+
+
+        public async Task CreateAsync(CategoryCreateDto dto)
+        {
+            Category entity = _mapper.Map<Category>(dto);
+
+            await _context.Categories.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task UpdateAsync(CategoryUpdateDto dto)
+        {
+            Category unchanged = await _context.Categories.FindAsync(dto.Id);
+
+            if (unchanged != null)
+            {
+                Category entity = _mapper.Map<Category>(dto);
+                _context.Entry(unchanged).CurrentValues.SetValues(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+
+        public async Task RemoveAsync(int id)
+        {
+            Category entity = await _context.Categories.FindAsync(id);
+
+            if (entity != null)
+            {
+                _context.Categories.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ ServiceLayer/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using ServiceLayer.Dtos.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryDto>> GetAllAsync();
+        Task<CategoryDto> GetByIdAsync(int id);
+        Task CreateAsync(CategoryCreateDto dto);
+        Task UpdateAsync(CategoryUpdateDto dto);
+        Task RemoveAsync(int id);
+    }
+}
