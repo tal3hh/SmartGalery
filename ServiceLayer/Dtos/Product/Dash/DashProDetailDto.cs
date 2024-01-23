80268f986fec88de42690b6ee9ee5fd3b62e5a54
tal3hh <suleymanovtaleh887@gmail.com>
2024-01-23 15:34:57 +0400

    editall

---
+++ ServiceLayer/Dtos/Product/Dash/DashProDetailDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Dtos.Product.Dash
+{
+    public class DashProDetailDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal OldPrice { get; set; }
+        public decimal Price { get; set; }
+        public bool IsStock { get; set; }
+        public int Count { get; set; }
+        public string? Color { get; set; }
+        public string? About { get; set; }
+
+        public List<string>? ProductImages { get; set; }
+        public List<string>? ProductDetails { get; set; }
+
+        public int commentCount { get; set; }
+        public decimal Rating { get; set; }
+
+        public string? CategoryName { get; set; }
+        public string? BrandName { get; set; }
+    }
+}
